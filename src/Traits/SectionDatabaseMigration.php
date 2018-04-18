329635f56f9195a2f9d1734adabad327dd00b671
Navid Sedehi <navid.sedehi@gmail.com>
2018-04-18 15:37:10 +0000

    Upload New File

---
+++ src/Traits/SectionDatabaseMigration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sedehi\Section\Traits;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\RefreshDatabaseState;
+
+trait SectionDatabaseMigration
+{
+
+    /**
+     * Define hooks to migrate the database before and after each test.
+     * @return void
+     */
+    public function runDatabaseMigrations(){
+
+        $this->artisan('section:migrate-fresh');
+        $this->app[Kernel::class]->setArtisan(null);
+        $this->beforeApplicationDestroyed(function(){
+
+            $this->artisan('section:migrate-rollback');
+            RefreshDatabaseState::$migrated = false;
+        });
+    }
+}
