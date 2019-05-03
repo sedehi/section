ef659a23cd5bfc27bcb40e10d801851f9c9e3e9b
Navid Sedehi <navid.sedehi@gmail.com>
2019-05-03 19:16:38 +0430

    command make:notification

--- src/Commands/SectionNotification.php
+++ src/Commands/SectionNotification.php
@@ -2,60 +2,28 @@
 
 namespace Sedehi\Section\Commands;
 
-use Illuminate\Console\Command;
-use File;
-use Illuminate\Console\DetectsApplicationNamespace;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Console\NotificationMakeCommand;
+use Illuminate\Support\Str;
+use Sedehi\Section\SectionOption;
 
-
-class SectionNotification extends Command
+class SectionNotification extends NotificationMakeCommand
 {
 
-    use DetectsApplicationNamespace, SectionsTrait;
+    use SectionOption;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'section:notification {section : The name of the section}  {name : The name of the notification}';
+    public function __construct(Filesystem $files){
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create a new notification class in section';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
+        parent::__construct($files);
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->makeDirectory($this->argument('section'), 'Notifications/');
+    protected function getDefaultNamespace($rootNamespace){
 
-        if (File::exists(app_path('Http/Controllers/'.ucfirst($this->argument('section')).'/Notifications/'.ucfirst($this->argument('name')).'.php'))) {
-            $this->error('notification already exists.');
-        } else {
-            $data = File::get(__DIR__.'/Template/notification.stub');
-
-            $data = str_replace('{{{name}}}', ucfirst($this->argument('name')), $data);
-            $data = str_replace('{{{section}}}', ucfirst($this->argument('section')), $data);
-            $data = str_replace('{{{appName}}}', $this->getAppNamespace(), $data);
-            File::put(app_path('Http/Controllers/'.ucfirst($this->argument('section')).'/Notifications/'.ucfirst($this->argument('name')).'.php'),
-                      $data);
-            $this->info('notification created successfully.');
+        $namespace = $rootNamespace.'\Http';
+        if(!is_null($this->option('section'))) {
+            $namespace = $namespace.'\Controllers\\'.Str::studly($this->option('section'));
         }
+
+        return $namespace.'\Notifications';
     }
 }
