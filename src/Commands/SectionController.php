478c64468e4e434d5d96bc9dc995ea47c5f8b8bc
Navid Sedehi <navid.sedehi@gmail.com>
2019-05-07 18:22:06 +0430

    command make:controller

--- src/Commands/SectionController.php
+++ src/Commands/SectionController.php
@@ -2,155 +2,203 @@
 
 namespace Sedehi\Section\Commands;
 
-use Artisan;
-use File;
-use Illuminate\Console\Command;
-use Illuminate\Console\DetectsApplicationNamespace;
+use Illuminate\Routing\Console\ControllerMakeCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
-
-class SectionController extends Command
+class SectionController extends ControllerMakeCommand
 {
 
-    use DetectsApplicationNamespace;
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'section:controller {section : The name of the section}  {name : The name of the controller} {--resource} {--upload} {--site} {--api} {--admin} {--crud} {--v= : Set api version} {--model= : Set model name}';
+    protected function getOptions(){
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create a new controller class in section';
+        $options = parent::getOptions();
+        $options = array_merge($options, [
+            ['section', 's', InputOption::VALUE_OPTIONAL, 'The name of the section'],
+            ['crud', null, InputOption::VALUE_NONE, 'Generate a crud controller class'],
+            ['upload', null, InputOption::VALUE_NONE, 'Generate a upload controller class'],
+            ['site', null, InputOption::VALUE_NONE, 'Generate a site controller class'],
+            ['admin', null, InputOption::VALUE_NONE, 'Generate a admin controller class'],
+            ['controller-version', null, InputOption::VALUE_OPTIONAL, 'Set version'],
+        ]);
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-
-    protected $controllerName;
-    protected $namespace;
-    protected $type;
-
-    public function __construct()
-    {
-        parent::__construct();
+        return $options;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
+    protected function getDefaultNamespace($rootNamespace){
 
-    private function init()
-    {
-
-        $this->makeDirectory($this->argument('section'), 'Controllers/');
-
-        $this->controllerName = ucfirst($this->argument('section')).'/Controllers/'.ucfirst($this->argument('name'));
-        $this->namespace      = $this->getAppNamespace().'Http\Controllers\\'.ucfirst($this->argument("section")).'\Controllers';
-
-        if ($this->option('site')) {
-            $this->makeDirectory($this->argument('section'), 'Controllers/Site/');
-            $this->controllerName = ucfirst($this->argument('section')).'/Controllers/Site/'.ucfirst($this->argument('name'));
-            $this->namespace      = $this->getAppNamespace().'Http\Controllers\\'.ucfirst($this->argument("section")).'\Controllers\Site';
-            $this->type           = '.site';
+        $namespace = $rootNamespace.'\Http\Controllers';
+        if(!is_null($this->option('section'))) {
+            $namespace .= '\\'.Str::studly($this->option('section')).'\\Controllers';
+        }
+        if($this->option('site')) {
+            $namespace .= '\\Site';
+        }
+        if($this->option('admin')) {
+            $namespace .= '\\Admin';
+        }
+        if($this->option('api')) {
+            $namespace .= '\\Api';
+        }
+        if(!is_null($this->option('controller-version'))) {
+            $namespace .= '\\'.Str::studly($this->option('controller-version'));
         }
 
-        if ($this->option('admin')) {
-            $this->makeDirectory($this->argument('section'), 'Controllers/Admin/');
-            $this->controllerName = ucfirst($this->argument('section')).'/Controllers/Admin/'.ucfirst($this->argument('name'));
-            $this->namespace      = $this->getAppNamespace().'Http\Controllers\\'.ucfirst($this->argument("section")).'\Controllers\Admin';
-            $this->type           = '.admin';
-        }
-
-        if ($this->option('api')) {
-            $this->makeDirectory($this->argument('section'), 'Controllers/Api/');
-
-            if ($this->option('v')) {
-                $this->controllerName = ucfirst($this->argument('section')).'/Controllers/Api/'.ucfirst($this->option('v')).'/'.ucfirst($this->argument('name'));
-                $this->namespace      = $this->getAppNamespace().'Http\Controllers\\'.ucfirst($this->argument("section")).'\Controllers\Api\\'.ucfirst($this->option('v'));
-            } else {
-                $this->controllerName = ucfirst($this->argument('section')).'/Controllers/Api/'.ucfirst($this->argument('name'));
-                $this->namespace      = $this->getAppNamespace().'Http\Controllers\\'.ucfirst($this->argument("section")).'\Controllers\Api';
-            }
-
-            $this->type = '.api';
-        }
+        return $namespace;
     }
 
-    public function handle()
-    {
-        $this->init();
+    protected function getStub(){
 
-        if (File::exists(app_path('Http/Controllers/'.$this->controllerName.'.php'))) {
-            return $this->error('controller already exists.');
+        $stub = null;
+        if($this->option('crud') && $this->option('model')) {
+            return __DIR__.'/stubs/controller-crud.stub';
         }
-
-
-        if ($this->option('upload')) {
-            if (!File::isDirectory(public_path('uploads/'.strtolower($this->argument('section')).'/'))) {
-                File::makeDirectory(public_path('uploads/'.strtolower($this->argument('section')).'/'), 0775, true);
-            }
+        if($this->option('upload') && $this->option('model')) {
+            return __DIR__.'/stubs/controller-upload.stub';
         }
+        if($this->option('parent')) {
+            $stub = '/stubs/controller.nested.stub';
+        }elseif($this->option('model')) {
+            $stub = '/stubs/controller.model.stub';
+        }elseif($this->option('invokable')) {
+            $stub = '/stubs/controller.invokable.stub';
+        }elseif($this->option('resource')) {
+            $stub = '/stubs/controller.stub';
+        }
+        if($this->option('api') && is_null($stub)) {
+            $stub = '/stubs/controller.api.stub';
+        }elseif($this->option('api') && !is_null($stub) && !$this->option('invokable')) {
+            $stub = str_replace('.stub', '.api.stub', $stub);
+        }
+        $stub = $stub ?? '/stubs/controller.plain.stub';
 
-        $data = null;
-        if ($this->option('resource')) {
-            if ($this->option('upload')) {
-                $data = File::get(__DIR__.'/stubs/controller/resource/AdminController-upload');
-            } else {
-                if ($this->option('crud')) {
-                    $data = File::get(__DIR__.'/stubs/controller/resource/AdminController');
-                } else {
-                    Artisan::call('make:controller', [
-                        'name'       => $this->controllerName,
-                        '--resource' => true,
+        return base_path('vendor/laravel/framework/src/Illuminate/Routing/Console/').$stub;
+    }
+
+    protected function buildClass($name){
+
+        $controllerNamespace = $this->getNamespace($name);
+        $replace             = [];
+        if($this->option('parent')) {
+            $replace = $this->buildParentReplacements();
+        }
+        if($this->option('model')) {
+            $replace = $this->buildModelReplacements($replace);
+        }
+        if($this->option('section')) {
+            $replace = $this->buildSectionReplacements($replace);
+        }
+        $replace                                             = $this->buildRequestReplacements($replace);
+        $replace                                             = $this->buildViewsReplacements($replace);
+        $replace                                             = $this->buildActionReplacements($replace);
+        $replace["use {$controllerNamespace}\Controller;\n"] = '';
+
+        return str_replace(array_keys($replace), array_values($replace), parent::buildClass($name));
+    }
+
+    protected function buildModelReplacements(array $replace){
+
+        $modelClass = $this->parseModel($this->option('model'));
+        if($this->option('section')) {
+            $modelClass = $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->option('section')).'\\Models\\'.Str::studly($this->option('model'));
+        }
+        if(!class_exists($modelClass)) {
+            if($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
+                if($this->option('section')) {
+                    $this->call('make:model', [
+                        'name'      => $this->option('model'),
+                        '--section' => $this->option('section')
                     ]);
+                }else {
+                    $this->call('make:model', ['name' => $modelClass]);
                 }
             }
-        } else {
-            Artisan::call('make:controller', ['name' => $this->controllerName]);
         }
 
-        if (!is_null($data)) {
-            $type = '';
-            switch ($this->type) {
-                case '.site':
-                    $type = 'Site\\';
-                    break;
-                case '.api':
-                    $type = 'Api\\';
-                    break;
-                case '.admin':
-                    $type = 'Admin\\';
-                    break;
-            }
+        return array_merge($replace, [
+            'DummyFullModelClass' => $modelClass,
+            'DummyModelClass'     => class_basename($modelClass),
+            'DummyModelVariable'  => lcfirst(class_basename($modelClass)),
+        ]);
+    }
 
-            $data = str_replace('{{{name}}}', ucfirst($this->argument('name')), $data);
-            $data = str_replace('{{{onlyName}}}', str_replace("Controller", "", ucfirst($this->argument('name'))), $data);
-            $data = str_replace('{{{type}}}', $type, $data);
-            $data = str_replace('{{{viewType}}}', $this->type, $data);
-            $data = str_replace('{{{section}}}', ucfirst($this->argument('section')), $data);
+    protected function buildSectionReplacements($replace){
 
-            if ($this->option('model')) {
-                $data = str_replace('{{{model}}}', studly_case($this->option('model')), $data);
-            } else {
-                $data = str_replace('{{{model}}}', ucfirst($this->argument('section')), $data);
-            }
+        $section = Str::studly($this->option('section'));
 
-            $data = str_replace('{{{sectionLower}}}', strtolower($this->argument('section')), $data);
-            $data = str_replace('{{{nameLower}}}', strtolower($this->argument('name')), $data);
-            $data = str_replace('{{{appName}}}', $this->getAppNamespace(), $data);
-            $data = str_replace('{{{namespace}}}', $this->namespace, $data);
-            File::put(app_path('Http/Controllers/'.$this->controllerName.'.php'), $data);
+        return array_merge($replace, [
+            'DummySectionNormal' => $section,
+            'DummySectionLower'  => strtolower($section),
+        ]);
+    }
+
+    protected function buildViewsReplacements($replace){
+
+        if($this->option('section')) {
+            $path = Str::studly($this->option('section')).'.views.'.str_replace('\\', '', strtolower($this->type())).'.'.strtolower($this->option('section'));
+        }else {
+            $path = 'views.'.strtolower($this->nameWithoutController());
         }
 
-        $this->info('controller created successfully.');
+        return array_merge($replace, [
+            'DummyViewPath' => $path,
+        ]);
+    }
+
+    protected function buildActionReplacements($replace){
+
+        if($this->option('section')) {
+            $path = Str::studly($this->option('section')).'\\Controllers\\'.Str::studly($this->type()).Str::studly($this->argument('name'));
+        }else {
+            $path = Str::studly($this->argument('name'));
+        }
+
+        return array_merge($replace, [
+            'DummyAction' => $path,
+        ]);
+    }
+
+    protected function buildRequestReplacements($replace){
+
+        if($this->option('section')) {
+            $requestClass = $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->option('section')).'\\Requests\\'.Str::studly($this->nameWithoutController()).'Request';
+            if(!class_exists($requestClass)) {
+                if($this->confirm("A {$requestClass} Request does not exist. Do you want to generate it?", true)) {
+                    if($this->option('section')) {
+                        $this->call('make:request', [
+                            'name'      => Str::studly($this->nameWithoutController()).'Request',
+                            '--section' => $this->option('section')
+                        ]);
+                    }else {
+                        $this->call('make:request', ['name' => Str::studly($this->nameWithoutController()).'Request']);
+                    }
+                }
+            }
+        }
+
+        return array_merge($replace, [
+            'DummyFullRequestClass' => ($this->option('section')) ? $requestClass : 'Illuminate\Http\Request',
+            'DummyRequestClass'     => ($this->option('section')) ? Str::studly($this->nameWithoutController()).'Request' : 'Request',
+            'DummyTypeLower'        => str_replace('\\', '', strtolower($this->type())),
+            'DummyTypeNormal'       => $this->type(),
+        ]);
+    }
+
+    protected function type(){
+
+        if($this->option('api')) {
+            return 'Api\\';
+        }elseif($this->option('site')) {
+            return 'Site\\';
+        }elseif($this->option('admin')) {
+            return 'Admin\\';
+        }else {
+            return '';
+        }
+    }
+
+    protected function nameWithoutController(){
+
+        return str_replace('Controller', '', $this->argument('name'));
     }
 
 }
