6da8b6411989cc72e0c3843a669f5808036e6ff0
Navid Sedehi <navid.sedehi@gmail.com>
2020-03-13 14:59:32 +0330

    Update SectionView.php

--- src/Console/SectionView.php
+++ src/Console/SectionView.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\File;
 
 class SectionView extends Command
 {
-    use DetectsApplicationNamespace;
-
     /**
      * The name and signature of the console command.
      *
